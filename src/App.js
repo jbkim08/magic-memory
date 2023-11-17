d64671b5fa755914d4c8c6768664b53d5fec5e43
JB <drv98@naver.com>
2023-11-17 10:02:01 +0900

    카드섞기

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+
+const cardImages = [
+  { src: '/img/helmet-1.png' },
+  { src: '/img/potion-1.png' },
+  { src: '/img/ring-1.png' },
+  { src: '/img/scroll-1.png' },
+  { src: '/img/shield-1.png' },
+  { src: '/img/sword-1.png' },
+];
 
 function App() {
+  //카드와 턴수 스테이트 만들기
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+
+  const shuffleCards = () => {
+    //카드 섞기
+    const shuffleCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+    //섞은카드 저장
+    setCards(shuffleCards);
+    setTurns(0); //턴수 0
+  };
+
+  console.log(cards, turns);
+
   return (
     <div className="App">
       <h1>Magic Match</h1>
-      <button>New Game</button>
+      <button onClick={shuffleCards}>New Game</button>
     </div>
   );
 }
 
-export default App
+export default App;
